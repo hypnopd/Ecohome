1bf140a0dac507be38eddb49ef8f6391b6f7aa80
Jozef Zigmund <jzigmund@redhat.com>
2012-09-11 19:47:24 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecohome::Application.config.secret_token = 'a88f935cec1d23250569aaead444eb6db6413f294824c2899d01fe4744d344ad645af07c1cc509776baf42e4a80e9f3819d0c02a7964c85c3f5ce0f8c15edcc7'
